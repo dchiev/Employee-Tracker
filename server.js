9e9f7bada06f2f082cae756d8a232d39d7fdd53c
Dan Chiev <danchiev2@Gmail.com>
2020-11-17 23:45:50 -0500

    tables are now joined. Add employee working

--- server.js
+++ server.js
@@ -1,26 +1,29 @@
+
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var cTable = require("console.table");
 
-require('dotenv').config()
+require("dotenv").config();
 
- 
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: process.env.DB_password,
+  database: "employee_DB",
+});
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: process.env.DB_password,
-    database: "employee_DB"
-  });
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId + "\n");
+  start();
+});
 
-  connection.connect(function(err){
-    if (err) throw err;
-    console.log("connected as id "+ connection.threadId + "\n");
-    viewDepartments();
-})
 
- function start() {
-    inquirer.prompt({
+
+function start() {
+  inquirer
+    .prompt({
       type: "list",
       message: "What would you like to do?",
       name: "start",
@@ -32,40 +35,107 @@ const connection = mysql.createConnection({
         "Add Department",
         "Add Role",
         "Update Employee Roles",
+        "Exit",
       ],
     })
-};
+    .then(function (result) {
+      console.log(result.start);
 
-function viewEmployees(){
-  let query= "SELECT * FROM employees";
-  connection.query(query,function(err,res){
-    if (err) 
-    throw err;
+      switch (result.start) {
+        case "View all employees":
+          viewEmployees();
+          break;
+        case "View all roles":
+          viewRoles();
+          break;
+        case "View all departments":
+          viewDepartments();
+          break;
+        case "Add Employee":
+          addEmployee();
+          break;
+        case "Exit":
+          console.log("You've Exit the application!");
+          connection.end();
+      }
+    });
+}
+
+function viewEmployees() {
+  let query = `SELECT e.id,
+               CONCAT (e.first_name," ", e.last_name) AS "Full Name",
+              r.salary, 
+              r.title, 
+              IF (CONCAT (e2.first_name," ", e2.last_name) IS NULL, "No Manager",CONCAT (e2.first_name," ", e2.last_name) ) AS "Manager Name" 
+               FROM employees AS e
+               LEFT JOIN roles AS r
+               ON e.role_id = r.id 
+               LEFT JOIN employees AS e2
+               ON e.manager_id = e2.id `;
+  connection.query(query, function (err, res) {
+    if (err) throw err;
     console.table(res);
     start();
   });
-};
+}
 
-function viewRoles(){
+function viewRoles() {
   let query = "SELECT * FROM roles";
-  connection.query(query, function (err,res){
-    if (err)
-    throw err;
+  connection.query(query, function (err, res) {
+    if (err) throw err;
     console.table(res);
     start();
   });
-};
+}
 
 function viewDepartments() {
-  let query = "SELECT * FROM department";
-  connection.query (query, function (err,res){
-    if (err)
-    throw err;
+  let query = `SELECT d.id, d.name, r.salary
+              FROM department AS d
+              LEFT JOIN roles AS r
+              ON d.id = r.department_id`;
+  connection.query(query, function (err, res) {
+    if (err) throw err;
     console.table(res);
     start();
   });
-};
+}
 
-/* function addEmployee () {
-  let query = "INSERT INTO EMPLOYEES "
-} */
+function addEmployee() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "firstName",
+        message: "What is the employee's first name?",
+      },
+      {
+        type: "input",
+        name: "lastName",
+        message: "What is the employee's last name?",
+      },
+      {
+        type: "list",
+        name: "role",
+        message: "What is the employee's job title?",
+        choices: ["IT Lead", "HR Lead", "Graphic Designer"],
+      },
+    ])
+    .then(function (answers) {
+      console.log(answers);
+      var role;
+      switch
+      role=1:
+      var newEmployeeData = {
+        "first_name": answers.firstName,
+        "last_name": answers.lastName,
+        "role_id": role
+      };
+      
+      connection.query(`INSERT INTO employees SET ?`, newEmployeeData, function (err, res) {
+        console.log("Success!");
+        start();
+    ;
+    });
+})};
+
+//determine department ID with switch case 
